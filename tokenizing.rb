a1a62eca9da9b02be3425b217b0f9c1123ba9a97
beyamor <beyamor@gmail.com>
2014-02-01 18:34:03 -0800

    Parsing strings as a single token

--- tokenizing.rb
+++ tokenizing.rb
@@ -1,6 +1,6 @@
 module Tokenizing
-	SPECIAL_SYMBOLS	= ["=", ";", "(", ")", "{", "}", "\"", ","]
-	WHITESPACE	= [" ", "\n", "\r", "\t"]
+	SPECIAL_SYMBOLS		= ["=", ";", "(", ")", "{", "}", "\"", ","]
+	WHITESPACE		= [" ", "\n", "\r", "\t"]
 
 	def Tokenizing.tokenize(text)
 		tokens	= []
@@ -13,13 +13,30 @@ module Tokenizing
 			end
 		end
 
-		text.chars.each do |char|
-			if SPECIAL_SYMBOLS.member? char
+		text = text.chars.to_a
+		until text.empty?
+			char = text.shift
+
+			if char == "\""
+				push_token.call
+				token = "\""
+				while true
+					throw :missing_string_terminator if text.empty?
+					char = text.shift
+					break if char == "\""
+					token << char
+				end
+				token << "\""
+				push_token.call
+
+			elsif SPECIAL_SYMBOLS.member? char
 				push_token.call
 				token = char
 				push_token.call
+
 			elsif WHITESPACE.member? char
 				push_token.call
+
 			else
 				token ||= ""
 				token << char
