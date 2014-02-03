251cfcc40604c18adb0d626e4a5036982366c8c1
beyamor <beyamor@gmail.com>
2014-02-03 12:47:00 -0800

    Passing the script as a command line arg

--- main.rb
+++ main.rb
@@ -3,7 +3,7 @@ require_relative "parsing"
 require_relative "primitives"
 require_relative "evaling"
 
-contents	= File.read "scripts/helloworld.derp"
+contents	= File.read ARGV[0]
 tokens		= Tokenizing.tokenize contents
 parse_result	= Parsing.parse tokens
 Evaling.run parse_result
